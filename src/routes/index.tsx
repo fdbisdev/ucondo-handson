9aafb8e8243cc517d6b1555d75de1eecf9b22d16
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2023-06-15 20:27:01 -0300

    chore: add navigation system

---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from '../pages/Home';
+import NewRevenue from '../pages/NewRevenue';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+const Routes: React.FC = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}
+                initialRouteName='Home'
+            >
+                <Stack.Group>
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="NewRevenue" component={NewRevenue} />
+                </Stack.Group>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes;
